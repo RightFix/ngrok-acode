ca72a7fcc14fb9bd38f62ad8728aa87a78ebe7a4
Right Fix <righteousnessude@gmail.com>
2025-12-11 09:39:38 +0100

    update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,8 @@ apk update && apt upgrade
 apk add wget unzip
 
 cd ..
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm64.zip
+
+wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-arm64.zip -O ngrok.zip
 
 unzip ngrok-stable-linux-arm64.zip
 rm ngrok-stable-linux-arm64.zip
@@ -16,7 +17,6 @@ mv ngrok /usr/bin
 
 echo -e "\e[1;32mNgrok installed sucessfull!"
 echo "Run : ngrok "
-echo " To use ngrok"
 echo "To get the version  - ngrok version "
 echo "To configure - ngrok config add-authtoken <token>"
 
