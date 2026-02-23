6ecbbfa7cbbb9f2e107cec68be6621553c52b72d
RightFix <righteousnessude@gmail.com>
2026-02-23 02:10:23 +0100

    little changes

--- install.sh
+++ install.sh
@@ -6,15 +6,13 @@ rm ../usr/bin/ngrok
 
 apk update && apk upgrade 
 apk add wget 
-
-cd ..
+ 
 
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-arm64.tgz -O ngrok.tgz
 
 tar xvzf  ngrok.tgz
 rm ngrok.tgz
 
-cd
 mv ngrok ../usr/bin
 
 echo -e "\e[1;32mNgrok installed sucessfull!"
